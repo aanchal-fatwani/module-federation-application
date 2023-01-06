6c9afc36e2933b0956003ba40f858267a24604e2
aanchal-fatwani <fatwaniaanchal@gmail.com>
2023-01-06 19:57:10 +0530

    feat(container): Added lazy loading of MFE modules

--- container/src/App.js
+++ container/src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Auth from "./remote_modules/auth";
 import Header from "./remote_modules/header";
-import Landing from "./remote_modules/landing";
+// import Auth from "./remote_modules/auth";
+// import Landing from "./remote_modules/landing";
+
+const Auth = lazy(()=>import("./remote_modules/auth"))
+const Landing = lazy(()=>import("./remote_modules/landing"))
 
 const App = () => {
   return (
     <div>
       <Header />
       <div style={{ width: "100%", height: "calc(100vh - 60px)" }}>
+        <Suspense fallback={<div>Loading..</div>}>
         <Routes>
           <Route path="/auth/*" element={<Auth />} />
           <Route path="/" element={<Landing />} />
         </Routes>
+        </Suspense>
       </div>
     </div>
   );
